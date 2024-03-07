77910e70a989745c8e0df49247cc36867fde4890
nali-kang <bsk9212@gmail.com>
2024-03-07 22:12:45 +0900

    fix(program): title 수정

--- components/MacProgram.vue
+++ components/MacProgram.vue
@@ -2,13 +2,18 @@
   <article
     class="w-full h-full bg-gray-800 rounded-lg absolute border-gray-600 border-[0.1px]"
   >
-    <div class="absolute px-3 flex gap-2 top-3">
-      <button
-        class="bg-red-600 rounded-full w-3 h-3"
-        @click="removeProgram"
-      ></button>
-      <button class="bg-yellow-600 rounded-full w-3 h-3"></button>
-      <button class="bg-green-600 rounded-full w-3 h-3"></button>
+    <div class="absolute px-3 flex w-full">
+      <div class="flex gap-2 py-3 items-center">
+        <button
+          class="bg-red-600 rounded-full w-3 h-3"
+          @click="removeProgram"
+        ></button>
+        <button class="bg-yellow-600 rounded-full w-3 h-3"></button>
+        <button class="bg-green-600 rounded-full w-3 h-3"></button>
+      </div>
+      <p class="grow flex items-center justify-center text-gray-400">
+        {{ program }}
+      </p>
     </div>
   </article>
 </template>
