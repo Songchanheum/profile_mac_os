128531816b07726c8ba8a8211193ba66fb56446e
chsong-alticast <bsk9212@gmail.com>
2024-03-07 15:23:15 +0900

    chore(image): resize

--- layouts/desktopLayout.vue
+++ layouts/desktopLayout.vue
@@ -2,7 +2,7 @@ import macHeader from '~/components/macHeader.vue'; import component from
 '../.nuxt/types/vue-shim'; import { MacHeader } from '../.nuxt/components';
 <template>
   <div
-    class="bg-[url('/assets/images/light-bg.png')] w-[100wv] h-[100vh] bg-cover"
+    class="bg-[url('/assets/images/dark-bg.png')] w-[100wv] h-[100vh] bg-cover"
   >
     <MacHeader :isDesktop="true" />
     <slot />
